f220158ac301e784e073897583df68bc84eaffb5
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2022-05-18 06:21:43 +0630

    ktp

--- Scripts/reward.js
+++ Scripts/reward.js
@@ -26,6 +26,14 @@
     });
 
     BindRewardPrize();
+
+    $('#productphoto').on('click', function () {
+        $('#productupload').click();
+    });
+
+    $('#quotationphoto').on('click', function () {
+        $('#quotationupload').click();
+    });
 }
 
 function BindRewardPrize() {
@@ -75,14 +83,44 @@ function AddNewPrize() {
 
     $(".iziModal-header-title").html('Add Reward Prize');
     $("#RewardPrizeModal").iziModal('open');
+
+    $("#txtItemCode").focus();
+
     BindProductCategory();
     $("#HMode").val('New');
 }
 
-function ClearReward() {
-    
-}
-
 function BindProductCategory() {
     BindDropdown({}, $("#HGetProductCategory").val(), 'ddlCategory', 'ProductCategoryID', 'ProductCategoryName','',true);
+}
+
+function readURL(input, ctrl) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#' + ctrl)
+                .attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function RewardPrizeClose() {
+    $("#RewardPrizeModal").iziModal('close');
+}
+
+function ClearReward() {
+    $("#productphoto").attr("src", $("#imgnophoto").val());
+    $("#quotationphoto").attr("src", $("#imgnophoto").val());
+    $("#rdoInactive").prop('checked', true);
+    $("#txtItemCode").val('');
+    $("#ddlCategory").val('');
+    $("#txtProductDescription").val('');
+    $("#txtPoints").val('');
+    $("#txtValidTill").val('');
+    $('#txtDetails').val('');
+    $("#txtUnitCost").val('');
+    $("#txtSupplierName").val('');
 }
