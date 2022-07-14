6bddb2d7974bccaf1bfb714b64937768eecc7f59
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2022-07-14 16:14:43 +0800

    ktp

--- Controllers/PartnerController.cs
+++ Controllers/PartnerController.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using VPPModel;
@@ -20,6 +22,48 @@ namespace VPPWeb.Controllers
         public ActionResult PartnerList()
         {
             return View();
+        }
+
+        [HttpPost]
+        public string CheckRevisePartnerImport()
+        {
+            MessageBL messageBL = new MessageBL();
+            PartnerModel partnerModel = new PartnerModel();
+            if (Request.Files["VipPartnerReviseUpload"].FileName.EndsWith(".xlsx"))
+            {
+                Stream stream = Request.Files["VipPartnerReviseUpload"].InputStream;
+
+                CommonFunction commonFunction = new CommonFunction();
+                DataTable csvTable = commonFunction.ExceltoDatatable(stream);
+
+                if (!csvTable.Columns.Contains("Register_No") || !csvTable.Columns.Contains("Company_Name") ||                  
+                    !csvTable.Columns.Contains("FirstName") || !csvTable.Columns.Contains("LastName") ||
+                    !csvTable.Columns.Contains("Phone") || !csvTable.Columns.Contains("Email") ||
+                    !csvTable.Columns.Contains("Date_Of_Event") || !csvTable.Columns.Contains("DSS_EventCode") ||
+                    !csvTable.Columns.Contains("EventName") || !csvTable.Columns.Contains("Country2") ||
+                    !csvTable.Columns.Contains("RewardPoints"))
+                {
+                    MessageModel messageModel = new MessageModel
+                    {
+                        MessageID = "E015"
+                    };
+                    return messageBL.Message_Select(messageModel);
+                }
+
+                partnerModel.ReviseImportJson = commonFunction.DataTableToJSONWithJSONNet(csvTable);
+
+                PartnerBL partnerBL = new PartnerBL();
+
+                return partnerBL.CheckRevisePartnerImport(partnerModel);
+            }
+            else
+            {
+                MessageModel messageModel = new MessageModel
+                {
+                    MessageID = "E014"
+                };
+                return messageBL.Message_Select(messageModel);
+            }
         }
 
         [HttpPost]
@@ -122,6 +166,173 @@ namespace VPPWeb.Controllers
                 return messageBL.Message_Select(messageModel);
             }
 
+        }
+
+        [HttpPost]
+        public string PartnerReviseImportConfirm()
+        {
+            MessageBL messageBL = new MessageBL();
+            string j1 = HttpContext.Request.Params.Get("PartnerModel");
+            PartnerModel partnerModel = JsonConvert.DeserializeObject<PartnerModel>(j1);
+
+            try
+            {
+                if (Request.Files["VipPartnerReviseUpload"].FileName.EndsWith(".xlsx"))
+                {
+                    Stream stream = Request.Files["VipPartnerReviseUpload"].InputStream;
+                    CommonFunction commonFunction = new CommonFunction();
+                    DataTable csvTable = commonFunction.ExceltoDatatable(stream);
+
+                    partnerModel.FileName = Request.Files["VipPartnerReviseUpload"].FileName;
+                    partnerModel.ReviseImportJson = commonFunction.DataTableToJSONWithJSONNet(csvTable);
+
+                    PartnerBL partnerBL = new PartnerBL();
+
+                    partnerBL.Partner_ReviseImportConfirm(partnerModel);
+
+                    MessageModel messageModel = new MessageModel
+                    {
+                        MessageID = "I006"
+                    };
+                    return messageBL.Message_Select(messageModel);
+                }
+                else
+                {
+                    MessageModel messageModel = new MessageModel
+                    {
+                        MessageID = "E014"
+                    };
+                    return messageBL.Message_Select(messageModel);
+                }
+            }
+            catch (Exception exception)
+            {
+                ErrorLogModel errorLogModel = new ErrorLogModel
+                {
+                    ErrorMessage = exception.Message,
+                    UpdatedBy = partnerModel.UpdatedBy
+                };
+
+                ErrorLogBL errorLogBL = new ErrorLogBL();
+                errorLogBL.ErrorLog_Insert(errorLogModel);
+
+                MessageModel messageModel = new MessageModel
+                {
+                    MessageID = "E014"
+                };
+                return messageBL.Message_Select(messageModel);
+            }
+
+        }
+
+        private string CreateRandomPassword(int length = 15)
+        {
+            // Create a string of characters, numbers, special characters that allowed in the password  
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            Random random = new Random();
+
+            // Select one random character at a time from the string  
+            // and create an array of chars  
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            return new string(chars);
+        }
+
+        public string SendWelcomeMail()
+        {
+            MessageBL messageBL = new MessageBL();
+            string j1 = HttpContext.Request.Params.Get("PartnerModel");
+            PartnerModel partnerModel = JsonConvert.DeserializeObject<PartnerModel>(j1);
+            try
+            {
+                PartnerBL partnerBL = new PartnerBL();
+                DataTable dtPartner = partnerBL.VIPPartner_SelectDT(partnerModel);
+
+                DataTable dtPartnerAfterMail = new DataTable();
+                dtPartnerAfterMail.Columns.Add("Register_No");
+                dtPartnerAfterMail.Columns.Add("LoginPassword");
+                dtPartnerAfterMail.Columns.Add("WelcomeMail");
+
+                for (int i = 0; i < dtPartner.Rows.Count; i++)
+                {
+                    try
+                    {
+                        MailMessage mail = new MailMessage();
+                        SmtpClient SmtpServer = new SmtpClient();
+                        //mail.From = new MailAddress("kyawthetpaing.mm@gmail.com");
+                        mail.From = new MailAddress("dlinkvip@gmail.com");
+                        mail.IsBodyHtml = true;
+                        mail.To.Add(dtPartner.Rows[i]["Email"].ToString());
+                        mail.Subject = "Welcome aboard | D-Link Registered Partner Rewards Program";
+                        string password = CreateRandomPassword(8);
+                        mail.Body = "<html><body>Dear " + dtPartner.Rows[i]["LastName"] + " " + dtPartner.Rows[i]["FirstName"] + "" + ",<br/>"
+                            + "&emsp;&emsp;Thank you so much for joining D-Link Registered Partner Rewards Program! We’re so excited to have you on board and can’t wait to get to know and serve you.<br/><br/>"
+                            + "&emsp;&emsp;We invite you to log in at http://viplogin.dlink.com.sg:88/VipPartner and access the exclusive, members-only resources.<br/><br/>"
+                            + "&emsp;&emsp;For security reason, please change the default password.</p><br/>"
+                            + "&emsp;&emsp;Login Name: " + dtPartner.Rows[i]["Email"] + "<br/>"
+                            + "&emsp;&emsp;Default Password: " + password + "<br/><br/>"
+                            + "&emsp;&emsp;Should you need any assistance or have questions, feel free to contact us.<br/><br/>"
+                            + "&emsp;&emsp;Thank you for being a part of D-Link Registered Partner family.<br/><br/>"
+                            + "&emsp;&emsp;Best Regards,<br/><br/>"
+                            + "&emsp;&emsp;Jacky Chang<br/>"
+                            + "&emsp;&emsp;President of D-Link ASEAN"
+                            + "</body></html>";
+                        SmtpServer.UseDefaultCredentials = false;
+                        //NetworkCredential NetworkCred = new NetworkCredential("kyawthetpaing.mm@gmail.com", "pjeqdhmncmktuatd");
+                        NetworkCredential NetworkCred = new NetworkCredential("dlinkvip@gmail.com", "vfgzvvdmfdmsdgxn");
+                        SmtpServer.Credentials = NetworkCred;
+                        SmtpServer.EnableSsl = true;
+                        SmtpServer.Port = 587;
+                        SmtpServer.Host = "smtp.gmail.com";
+                        SmtpServer.Send(mail);
+
+                        dtPartnerAfterMail.Rows.Add();
+                        dtPartnerAfterMail.Rows[i]["Register_No"] = dtPartner.Rows[i]["Register_No"].ToString();
+                        dtPartnerAfterMail.Rows[i]["LoginPassword"] = password;
+                        dtPartnerAfterMail.Rows[i]["WelcomeMail"] = "1";
+
+                    }
+                    catch (Exception)
+                    {
+                        dtPartnerAfterMail.Rows.Add();
+                        dtPartnerAfterMail.Rows[i]["Register_No"] = dtPartner.Rows[i]["Register_No"].ToString();
+                        dtPartnerAfterMail.Rows[i]["LoginPassword"] = "";
+                        dtPartnerAfterMail.Rows[i]["WelcomeMail"] = "2";
+                    }
+                }
+
+                CommonFunction commonFunction = new CommonFunction();           
+
+                partnerModel.ImportJson = commonFunction.DataTableToJSONWithJSONNet(dtPartnerAfterMail);
+
+                partnerBL.PartnerRegister_AfterWelcomeMail(partnerModel);
+
+                MessageModel messageModel = new MessageModel
+                {
+                    MessageID = "I008"
+                };
+                return messageBL.Message_Select(messageModel);
+            }
+            catch (Exception exception)
+            {
+                ErrorLogModel errorLogModel = new ErrorLogModel
+                {
+                    ErrorMessage = exception.Message,
+                    UpdatedBy = partnerModel.UpdatedBy
+                };
+
+                ErrorLogBL errorLogBL = new ErrorLogBL();
+                errorLogBL.ErrorLog_Insert(errorLogModel);
+
+                MessageModel messageModel = new MessageModel
+                {
+                    MessageID = "E018"
+                };
+                return messageBL.Message_Select(messageModel);
+            }        
         }
     }
 }
--- Scripts/partner.js
+++ Scripts/partner.js
@@ -5,8 +5,17 @@
         $('#VipPartnerUpload').click();
     });
 
+    $('#btnReviseUpload').on('click', function () {
+        $('#VipPartnerReviseUpload').val('');
+        $('#VipPartnerReviseUpload').click();
+    });
+
     $("#VipPartnerUpload").change(function () {
         StandardImportCheck();
+    });
+
+    $("#VipPartnerReviseUpload").change(function () {
+        ReviseImportCheck();
     });
 
     $("#PartnerImportModal").iziModal({
@@ -21,14 +30,145 @@
         width: '80%'
     });
 
+    $("#PartnerReviseImportModal").iziModal({
+        title: 'VIP Partner Import',
+        TransitionIn: 'FadeInRight',
+        theme: 'light',
+        headerColor: '#008fa2',
+        padding: 10,
+        top: 20,
+        overlayClose: false,
+        zindex: 1100,
+        width: '80%'
+    });
+
+    $('#btnWelcomeEmail').on('click', function () {
+        ShowConfirmMessage('Q004','SendWelcomeEmail')
+    });
+
+    $('#btnPartnerReviseImportClose').on('click', function () {
+        PartnerReviseImportClose();
+    });
+
+    $("#btnPartnerReviseImportConfirm").on('click', function () {
+        PartnerReviseImportConfirm();
+    });
+
+    $("#btnSearch").on('click', function () {
+        BindPartner();
+    });
+
     BindPartner();
+    BindCountry();
+}
+
+function BindCountry() {
+    var obj;
+    if ($("#hVIPUserRole").val() == '1') {
+        obj = {};
+    } else {
+        obj = {
+            CountryList: $("#hCountryList").val(),
+        }
+    }
+
+    CalltoApiController($("#HGetCountry").val(), obj, 'CountryResponse');
+}
+
+function CountryResponse(response) {
+    DropdownResponse(response, 'SCountry', 'CountryID', 'CountryName', '', true);
+}
+
+function ReviseImportCheck() {
+    $('#divloader').show();
+
+    var formdata = new FormData();
+    var fileRW = $('#VipPartnerReviseUpload')[0];
+    formdata.append('VipPartnerReviseUpload', fileRW.files[0]);
+    $.ajax({
+        url: $("#HCheckReviseImport").val(),
+        type: "POST",
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: formdata,
+        success: function (data) {
+            var jsonResult = JSON.parse(data);
+            var messageID = jsonResult[0].MessageID;
+
+            if (messageID != '0') {
+                MessageResponse(data, messageID)
+                return;
+            } else {
+                $("#PartnerReviseImportModal").iziModal('open');
+
+                var saveEnable = true;
+
+                $('#tblPartnerRevisePopUp').DataTable({
+                    scrollY: '290px',
+                    scrollX: true,
+                    scrollCollapse: true,
+                    fixedColumns: {
+                        left: 4,
+                    },
+                    fixedHeader: true,
+                    autoWidth: false,
+                    data: jsonResult,
+                    datasrc: "",
+                    destroy: true,
+                    searching: false,
+                    "bPaginate": false,
+                    "ordering": false,
+                    "columns": [
+                        { "data": "SEQ", "className": "align-center", width: "5%" },
+                        { "data": "Status", "className": "align-center", width: "5%" },
+                        { "data": "ErrorMessage", width: "10%" },
+                        { "data": "Register_No", width: "10%" },
+                        { "data": "Company_Name", width: "10%" },
+                        { "data": "FirstName", width: "10%" },
+                        { "data": "LastName", width: "10%" },
+                        { "data": "Phone", width: "10%" },
+                        { "data": "Email", width: "10%" },
+                        { "data": "DateOfEvent", width: "10%" },
+                        { "data": "DSSEventCode", width: "10%" },
+                        { "data": "EventName", width: "10%" },
+                        { "data": "CountryName", width: "10%" },
+                        { "data": "RewardPoints", width: "10%" },
+                    ],
+                    "rowCallback": function (row, data, index) {
+                        if (data.Status == "Error") {
+                            saveEnable = false;
+                            $('td', row).css('color', 'Red');
+                        }
+                    }
+                });
+
+                if (saveEnable == false) {
+                    $("#divconfirmrevise").hide();
+                }
+                else {
+                    $("#divconfirmrevise").show();
+                }
+            }
+        },
+        fail: function (data) {
+            ShowMessage('E007');
+        },
+        complete: function (data) {
+            $('#divloader').hide();
+        }
+    });
 }
 
 function BindPartner() {
     $('#tblVIPPartner tbody').empty();
 
     var obj = {
-
+        Company_Name: $("#StxtCompanyName").val(),
+        CountryID: $("#SCountry").children("option:selected").val(),
+        Email: $("#StxtEmail").val(),
+        Name: $("#StxtName").val(),
+        PartnerStatus: $("#SPartnerStatus").children("option:selected").val(),
     };
     CalltoApiController($("#HGetVIPPartner").val(), obj, 'GetVIPPartnerResponse');
 }
@@ -54,25 +194,44 @@ function GetVIPPartnerResponse(response) {
         "pageLength": 50,
         "ordering": false,
         "columns": [
-            { "data": "PartnerStatus", width: "5%", className: "align-center" },
-            { "data": "Register_No", width: "5%", className: "align-center" },
-            { "data": "Company_Name", width: "5%", className: "align-center" },
-            { "data": "Country", width: "5%", className: "align-center" },
-            { "data": "Address", width: "7%", className: "align-center" },
+            { "data": "PartnerStatus", width: "10%", className: "align-center" },
+            { "data": "Register_No", width: "10%", className: "align-center" },
+            { "data": "Company_Name", width: "10%"},
+            { "data": "Country", width: "10%"},
+            { "data": "Address", width: "10%" },
             { "data": "FirstName", width: "10%" },
-            { "data": "LastName", width: "20%" },
-            { "data": "Email", width: "20%" },
+            { "data": "LastName", width: "10%" },
+            { "data": "Email", width: "10%" },
+            { "data": "City", width: "10%"},
+            { "data": "ZipCode", width: "10%"},
+            { "data": "Location_No", width: "10%"},
+            { "data": "Partner_Type", width: "10%"},
+            { "data": "Business_Focus", width: "10%" },
+            { "data": "EmailNoti", width: "10%", className: "align-center" },
+            { "data": "Prefix", width: "10%", className: "align-center" },
+            { "data": "Phone", width: "10%" },
+            { "data": "Job_Title", width: "10%" },
+            { "data": "Website", width: "10%" },
         ],
         "columnDefs": [
             {
                 "targets": 0,
-                "data": "ItemCode",
+                "data": "PartnerStatus",
                 "render": function (data) {
                     var t1 = 'New';
                     var c1 = 'label-primary';
                     if (data == '2') {
-                        t1 = 'Welcome Email'
+                        t1 = 'Wel'
+                        c1 = 'label-success';
+                    } else if (data == '3') {
+                        t1 = 'Re1'
+                        c1 = 'label-inverse';
+                    } else if (data == '4') {
+                        t1 = 'Re2'
                         c1 = 'label-warning';
+                    } else if (data == '5') {
+                        t1 = 'RPM'
+                        c1 = 'label-danger';
                     }
                     return '<span class="pcoded-badge label '+ c1 +'">'+ t1 +'</span>';
                 },
@@ -129,14 +288,14 @@ function StandardImportCheck() {
                         { "data": "Company_Name", width: "10%" },
                         { "data": "Address", width: "10%" },
                         { "data": "City", width: "10%" },
-                        { "data": "ZipCode", "className": "align-center", width: "10%" },
+                        { "data": "ZipCode", width: "10%" },
                         { "data": "Country", width: "10%" },
                         { "data": "Website", width: "10%" },
                         { "data": "Location_No", width: "10%" },
                         { "data": "Partner_Type", width: "10%" },
                         { "data": "Business_Focus", width: "10%" },
-                        { "data": "EmailNoti", width: "10%" },
-                        { "data": "Prefix", width: "10%" },
+                        { "data": "EmailNoti", "className": "align-center", width: "10%" },
+                        { "data": "Prefix", "className": "align-center", width: "10%" },
                         { "data": "FirstName", width: "10%" },
                         { "data": "LastName", width: "10%" },
                         { "data": "Phone", width: "10%" },
@@ -187,6 +346,86 @@ function PartnerImportConfirm() {
 
     $.ajax({
         url: $("#HPartnerStandardImportConfirm").val(),
+        type: "POST",
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: formdata,
+        success: function (data) {
+            var jsonResult = JSON.parse(data);
+            var messageID = jsonResult[0].MessageID;
+
+            MessageResponse(data, messageID);
+            $("#PartnerImportModal").iziModal('close');
+
+            BindPartner();
+        },
+        fail: function (response) {
+            data = JSON.parse(response);
+            MessageResponse(response, data[0].MessageID)
+        },
+        complete: function (data) {
+            $('#divloader').hide();
+            $("#VipPartnerUpload").val('');
+        }
+    });
+}
+
+function PartnerReviseImportClose() {
+    $("#PartnerReviseImportModal").iziModal('close');
+    $("#VipPartnerReviseUpload").val('');
+}
+
+function PartnerReviseImportConfirm() {
+    $('#divloader').show();
+
+    var obj = {
+        UpdatedBy: $("#hID").val(),
+    }
+
+    var formdata = new FormData();
+    var fileRW = $('#VipPartnerReviseUpload')[0];
+    formdata.append('VipPartnerReviseUpload', fileRW.files[0]);
+    formdata.append('PartnerModel', JSON.stringify(obj));
+
+    $.ajax({
+        url: $("#HPartnerReviseImportConfirm").val(),
+        type: "POST",
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: formdata,
+        success: function (data) {
+            var jsonResult = JSON.parse(data);
+            var messageID = jsonResult[0].MessageID;
+
+            MessageResponse(data, messageID);
+            $("#PartnerReviseImportModal").iziModal('close');
+
+            BindPartner();
+        },
+        fail: function (response) {
+            data = JSON.parse(response);
+            MessageResponse(response, data[0].MessageID)
+        },
+        complete: function (data) {
+            $('#divloader').hide();
+            $("#VipPartnerReviseUpload").val('');
+        }
+    });
+}
+
+function SendWelcomeEmail() {
+    $('#divloader').show();
+    var obj = {
+        SendBy: $("#hID").val(),
+    }
+
+    var formdata = new FormData();
+    formdata.append('PartnerModel', JSON.stringify(obj));
+
+    $.ajax({
+        url: $("#HSendWelcomeMail").val(),
         type: "POST",
         cache: false,
         contentType: false,
